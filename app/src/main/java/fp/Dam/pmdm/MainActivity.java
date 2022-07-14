f0ed10bb7c287ebf7b52a9bea4b07a0d5a1328f1
angela <angelablancoroca@gmail.com>
2022-07-14 12:04:54 +0200

    VIDEO 2

--- app/src/main/java/fp/Dam/pmdm/MainActivity.java
+++ app/src/main/java/fp/Dam/pmdm/MainActivity.java
@@ -9,18 +9,26 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity /*implements View.OnClickListener*/{
 
-    // ------ TEMA - 1 -------
-
+    // ------ VIDEO - 1 -------
     //EJERCICIO TRES FORMAS DE PULSAR BOTON
     //COLOCAR BOTON ANCLAR
     //TODOS LOS MËTODOS INTERESANTES DE LA VENTANA
 
+    //-------VIDEO - 2 ----------------------------------------- Tema - 3
+    //Recuperar info cuando está en segundo plano
+    //Se ejecuta un método antes del pause -- onSave
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { //Objeto que sirve para recupear el estado de la activity en una circusncia muy concreta
+        // En caso de ir para atrás o cerrar no puedo usar ese mecanismo de recuperación
+        //Solamente cuando pasa a segundo plano, cuando abro otra aplicacion y queda en pause
+        //el obejto Bundle es null
+        //Si quiero guardar el estado usar otros métodos -- ya los veremos
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         Toast.makeText(getApplicationContext(), "CREATE", Toast.LENGTH_SHORT).show(); //Devuelve el cartelito con mensaje
@@ -47,11 +55,6 @@ public class MainActivity extends AppCompatActivity /*implements View.OnClickLis
     }
 
 
-
-
-
-
-
     @Override
     protected void onStart() {
         super.onStart();
@@ -64,10 +67,39 @@ public class MainActivity extends AppCompatActivity /*implements View.OnClickLis
         Toast.makeText(getApplicationContext(), "RESUME", Toast.LENGTH_SHORT).show(); //Devuelve el cartelito con mensaje
     }
 
+
+
     @Override
     protected void onPause() {
         super.onPause();
         Toast.makeText(getApplicationContext(), "PAUSE", Toast.LENGTH_SHORT).show(); //Devuelve el cartelito con mensaje
+    }
+
+    /*
+   Método que se ejecuta despues del método onPause
+   Sirve para guardar cualquier cosa de l activity, el estado
+   Uso el objeto outState
+    */
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) { //Es un contenedor como un mapa, puedo meter culaquier dato
+        Toast.makeText(getApplicationContext(), "SAVE INSTANCE", Toast.LENGTH_SHORT).show(); //Devuelve el cartelito con mensaje
+        outState.putString("nombre", ((EditText) findViewById(R.id.editNombre)).getText().toString());//Clave -- dato guardado en editNombre, en el cuadro de texto
+        //Me retorna cualquier cosa por eso hago un cast
+        //invoco metodo toString para guardar cadena de texto, salvo el estado de mi actividad
+        super.onSaveInstanceState(outState);
+    }
+
+    /*
+    Metodo para retornar, recuperar lo anterior, en caso de destruir la aplicacion -- aparecería el cartel destroy
+    Esto se puede hacer en el create, es lo más normal y no usar este método -- pero hay problema el objeto es null entonces hay que usar otros métodos, que ya los veremos
+    //--- No funciona en el emulador, en el móvil probarlo:
+         - hay una opción que habría que activar -- interrumpir actividades , el android cada vez quue lleve una activudad a segundo plano la destruyr
+         entonces si funciona
+     */
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        ((EditText) findViewById(R.id.editNombre)).setText(savedInstanceState.getString("nombre"));
     }
 
     @Override
